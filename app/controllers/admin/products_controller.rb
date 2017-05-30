65f697b5c4949c749abe6133cdc3f1ed7716e2dc
huter2000 <19810511qcx@gmail.com>
2017-05-30 15:30:51 +0800

    change admin/product backed panel

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -29,12 +29,13 @@ class Admin::ProductsController < ApplicationController
     if @product.save
       redirect_to admin_products_path
     else
-      render:new
+      render :new
     end
   end
   private
 
   def product_params
     params.require(:product).permit(:title, :description, :quantity, :price, :image)
+
   end
 end
